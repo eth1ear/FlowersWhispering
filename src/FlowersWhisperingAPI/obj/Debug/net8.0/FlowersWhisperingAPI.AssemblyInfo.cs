9abc328bdbbf69392300f19eacd2c11458e38ea8
Doki.x1 <Ethereal.x1@outlook.com>
2024-08-04 19:42:19 +0800

    vsc

--- src/FlowersWhisperingAPI/obj/Debug/net8.0/FlowersWhisperingAPI.AssemblyInfo.cs
+++ src/FlowersWhisperingAPI/obj/Debug/net8.0/FlowersWhisperingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowersWhisperingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d57e7485bfd9897529a58404420798488cfcd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8efb9629346634ac7934d214802fe224c77226f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowersWhisperingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowersWhisperingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
