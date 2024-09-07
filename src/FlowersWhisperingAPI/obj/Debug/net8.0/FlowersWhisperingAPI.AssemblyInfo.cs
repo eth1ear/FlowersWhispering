4c39d9c66867527a14608cf4b564a89e0d02b4e0
SharkPepper-77777 <1816123272@qq.com>
2024-09-08 02:00:55 +0800

    爬新版

--- src/FlowersWhisperingAPI/obj/Debug/net8.0/FlowersWhisperingAPI.AssemblyInfo.cs
+++ src/FlowersWhisperingAPI/obj/Debug/net8.0/FlowersWhisperingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowersWhisperingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2916776b320da969691f58bfa5ebe31965fc487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa82042251dc059c662010feb24a585e16affd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowersWhisperingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowersWhisperingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
